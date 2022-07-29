b915d0c13fd383e0e3144162c9e9faa2c07fa17c
Matthew Fournier <matthew.fournier@pointercreative.com>
2022-07-28 21:59:25 -0400

    clone app

--- devops/clone-section/clone-section.js
+++ devops/clone-section/clone-section.js
@@ -3,15 +3,25 @@ const fs = require('fs-extra')
 const replace = require('replace-in-file')
 const { terminate, cancelKeys, input } = require('../helpers/helpers')
 
-// const updateFile = async (name, oldName, newName, newPath) => {
-//   const newFilePath = `${newPath}/${name.replace(oldName, newName)}`
-//   await fs.rename(`${newPath}/${name}`, newFilePath)
-//   return await replace({
-//     files: newFilePath,
-//     from: new RegExp(oldName, 'g'),
-//     to: newName
-//   })
-// }
+const cloneFile = async (path, sectionToCloneName, newSectionName) => {
+  const oldPath = path.replace('SECTION_NAME', sectionToCloneName)
+  const newPath = path.replace('SECTION_NAME', newSectionName)
+
+  const oldPathCheck = await fs.pathExists(oldPath)
+  if (!oldPathCheck) { throw Error(`<${oldPath}> does not exist`) }
+
+  const newPathCheck = await fs.pathExists(newPath)
+  if (newPathCheck) { throw Error(`<${newPath}> already exists`) }
+
+  await fs.copy(oldPath, newPath)
+  await replace({
+    files: newPath,
+    from: new RegExp(sectionToCloneName, 'g'),
+    to: newSectionName
+  })
+
+  return [oldPath, newPath]
+}
 
 ;(async () => {
   try {
@@ -19,40 +29,33 @@ const { terminate, cancelKeys, input } = require('../helpers/helpers')
 
     const sectionFiles = await fs.readdir('sections')
     term.cyan('Section folder to clone:')
-    const selectedOption = await term.gridMenu(sectionFiles.map(file => file.split('.')[0])).promise
+    const selectedOption = await term.gridMenu(
+      sectionFiles.map(file => file.split('.')[0])
+    ).promise
 
     const sectionToCloneName = selectedOption.selectedText
     const newSectionName = await input('\nNew Section Filename')
 
-    console.log(sectionToCloneName)
-    console.log(newSectionName)
+    const verifyInput = await input(`\nCreate ${newSectionName} section from ${sectionToCloneName}? (y/n)`)
+    const cloneGo = verifyInput.toLowerCase()
+    if (cloneGo !== 'yes' && cloneGo !== 'y') {
+      term.red('\nCanceled')
+      return terminate()
+    }
 
-    await Promise.allSettled([
+    const cloneRes = await Promise.allSettled([
       'sections/SECTION_NAME.liquid',
       'assets/SECTION_NAME.css',
       'assets/SECTION_NAME.js'
-    ].map(async (path) => {
-      console.log(path)
-    }))
+    ].map(async (path) => cloneFile(path, sectionToCloneName, newSectionName)))
 
-    // const oldPath = `sections/${existingFolder}`
-    // const newPath = `sections/${newSectionName}`
+    const message = cloneRes
+      .filter((res) => res.status === 'fulfilled')
+      .map((res) => `<${res.value[1]}> cloned from <${res.value[0]}>`)
+      .join('\n')
 
-    // const exists = await fs.pathExists(newPath)
-    // if (exists) { throw Error(`${newSectionName} already exists`) }
+    term.green(`\nSuccess!\n${message}`)
 
-    // const verifyInput = await input(`\nCreate ${newSectionName} section from ${existingFolder}? (y/n)`)
-    // const cloneGo = verifyInput.toLowerCase()
-    // if (cloneGo !== 'yes' && cloneGo !== 'y') {
-    //   term.red('\nCanceled')
-    //   return terminate()
-    // }
-
-    // await fs.copy(oldPath, newPath)
-    // const files = await fs.readdir(newPath)
-    // await Promise.all(files.map(file => updateFile(file, existingFolder, newSectionName, newPath)))
-
-    // term.green(`\nSuccess! ${existingFolder} has been cloned to ${newSectionName}.`)
     return terminate()
   } catch (error) {
     term.red(`\n${error}`)
