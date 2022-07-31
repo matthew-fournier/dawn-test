4698c669ee2a054bf952ae6fa378002900f92893
Matthew Fournier <matthewfournier3@gmail.com>
2022-07-30 22:23:35 -0400

    save

--- scripts/sections/section-test.js
+++ scripts/sections/section-test.js
@@ -1,8 +1,14 @@
+import cartAPI from '@cart/cart-api'
+
 console.log('hi')
 
 const x = 27
 
-document.addEventListener('DOMContentLoaded', () => {
-  alert('hi')
+document.addEventListener('DOMContentLoaded', async () => {
   console.log('softly')
+
+  const newDIv = document.createElement('div')
+  await cartAPI.cart()
+  newDIv.innerHTML = JSON.stringify(await cartAPI.getCartData())
+  document.body.prepend(newDIv)
 })
